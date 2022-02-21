e6c059bf9b29a32f1503996c3feeb04ad54ac01e
Silas Rafael <59030849+silasprd@users.noreply.github.com>
2022-02-20 21:38:29 -0300

    Delete todo.model.ts

--- src/models/todo.model.ts
+++
@@ -1,8 +0,0 @@
-export class Todo { // export diz que a classe é publica
-    
-    constructor( 
-        public id: Number,
-        public title: String,
-        public done: Boolean,
-        ) { }
-}
